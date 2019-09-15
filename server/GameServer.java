cc76904b08268e7dd1548e8751d60e6f6c3351b8
Alex <alxbly@rams.colostate.edu>
2019-09-15 16:44:45 -0600

    Class structure

--- server/GameServer.java
+++ server/GameServer.java
@@ -6,62 +6,74 @@ public class GameServer{
 
     static final int PORT_NUMBER = 8080;
     static final String RELATIVE_PATH = "../client/html";
-    static final String header = "HTTP/1.0 200 OK\nContent-Type: text/html\n\n";
+    static final String HEADER = "HTTP/1.0 200 OK\nContent-Type: text/html\n\n";
+    static final String DEFAULT_METHOD = "GET";
+    static final String DEFAULT_PAGE = "/index.html";
 
-    public static void main(String[] args){
-
-        try (ServerSocket serverListener = new ServerSocket(PORT_NUMBER)) { // Set up server to listen at PORT_NUMBER.
-
+    GameServer(){
+        try{
+            ServerSocket serverListener = new ServerSocket(PORT_NUMBER); // Set up server to listen at PORT_NUMBER.
             System.out.println("GameServer listening.");
 
             while (true) {
-
-                try (Socket clientSocket = serverListener.accept()){ // accept a connection from a client, fork socket for this connection.
-
-                    PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true); // sends data to client.
-                    InputStream socketInputStream = clientSocket.getInputStream(); // gets data from client.
-                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socketInputStream)); // stores data from client.
+                try{
+                    Socket clientSocket = serverListener.accept(); // accept a connection from a client, fork socket for this connection.
+                    InputStream InputStream = clientSocket.getInputStream(); // gets data from client.
+                    PrintWriter outputStream = new PrintWriter(clientSocket.getOutputStream(), true); // sends data to client.
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(InputStream)); // stores data from client.
 
                     String request = bufferedReader.readLine();
-                    System.out.println(request);
-                    String path;
+                    String method = DEFAULT_METHOD;
+                    String path = DEFAULT_PAGE;
 
                     try{
-                        path = request.split(" ")[1]; // get name of HTML page requested.
-                    }catch(ArrayIndexOutOfBoundsException e){
-                        path = "/index.html";
+                        String[] parts = request.split(" ");
+                        method = parts[0];
+                        path = parts[1];
+                    }
+                    catch(ArrayIndexOutOfBoundsException e){
                     }
 
                     if(path.equals("/")){
-                        path = "/index.html";
+                        path = DEFAULT_PAGE;
                     }
 
-                    File file = new File(RELATIVE_PATH + path);
+                    if(method.equals("GET")){
+                        File file = new File(RELATIVE_PATH + path);
 
-                    if(!file.exists()){
-                        out.println("404 file not found!");
+                        if(!file.exists()){
+                            outputStream.println("404 file not found!");
+                        }
+
+                        FileReader fileReader = new FileReader(file);
+                        BufferedReader fileBuffer = new BufferedReader(fileReader); // read the file into a buffer (reused).
+                        String response = HEADER; // append header to response.
+                        String line;
+
+                        while((line = fileBuffer.readLine()) != null){ // read until end of file.
+                            response += line; // append to response.
+                        }
+
+                        outputStream.println(response); // send response to client.
+                        fileBuffer.close();
+
                     }
-
-                    FileReader fileReader = new FileReader(file);
-                    bufferedReader = new BufferedReader(fileReader); // read the file into a buffer (reused).
-                    String response = header; // append header to response.
-                    String line;
-
-                    while((line = bufferedReader.readLine()) != null){ // read until end of file.
-                        response += line; // append to response.
-                    }
-
-                    out.println(response); // send response to client.
 
                     bufferedReader.close();
-                    out.close();
-
-                }catch(IOException e){}
-
+                    outputStream.close();
+                }
+                catch(IOException e){
+                }
             }
 
-        }catch(IOException e){}
-
+        }
+        catch(IOException e){
+        }
     }
 
+    public static void main(String[] args){
+
+        GameServer gameServer = new GameServer();
+        return 0;
+    }
 }
