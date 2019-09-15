d9a5f7e70d709c595b251b77e126e82dc00f4457
Alex <alxbly@rams.colostate.edu>
2019-09-15 17:05:13 -0600

    Refactored class

--- server/GameServer.java
+++ server/GameServer.java
@@ -10,6 +10,17 @@ public class GameServer{
     static final String DEFAULT_METHOD = "GET";
     static final String DEFAULT_PAGE = "/index.html";
 
+    ServerSocket serverListener;
+    Socket clientSocket;
+    InputStream inputStream;
+    PrintWriter outputStream;
+    BufferedReader bufferedReader;
+
+    String request;
+    String method;
+    String path;
+
+
     GameServer(){
         try{
             ServerSocket serverListener = new ServerSocket(PORT_NUMBER); // Set up server to listen at PORT_NUMBER.
@@ -17,63 +28,78 @@ public class GameServer{
 
             while (true) {
                 try{
-                    Socket clientSocket = serverListener.accept(); // accept a connection from a client, fork socket for this connection.
-                    InputStream InputStream = clientSocket.getInputStream(); // gets data from client.
-                    PrintWriter outputStream = new PrintWriter(clientSocket.getOutputStream(), true); // sends data to client.
-                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(InputStream)); // stores data from client.
-
-                    String request = bufferedReader.readLine();
-                    String method = DEFAULT_METHOD;
-                    String path = DEFAULT_PAGE;
-
-                    try{
-                        String[] parts = request.split(" ");
-                        method = parts[0];
-                        path = parts[1];
-                    }
-                    catch(ArrayIndexOutOfBoundsException e){
-                    }
-
-                    if(path.equals("/")){
-                        path = DEFAULT_PAGE;
-                    }
-
-                    if(method.equals("GET")){
-                        File file = new File(RELATIVE_PATH + path);
-
-                        if(!file.exists()){
-                            outputStream.println("404 file not found!");
-                        }
-
-                        FileReader fileReader = new FileReader(file);
-                        BufferedReader fileBuffer = new BufferedReader(fileReader); // read the file into a buffer (reused).
-                        String response = HEADER; // append header to response.
-                        String line;
-
-                        while((line = fileBuffer.readLine()) != null){ // read until end of file.
-                            response += line; // append to response.
-                        }
-
-                        outputStream.println(response); // send response to client.
-                        fileBuffer.close();
-
-                    }
-
-                    bufferedReader.close();
-                    outputStream.close();
+                    clientSocket = serverListener.accept(); // accept a connection from a client, fork socket for this connection.
+                    setUpConnection();
+                    parseRequest();
+                    handleRequest();
+                    tearDownConnection();
                 }
-                catch(IOException e){
+                catch(IOException | ArrayIndexOutOfBoundsException e){
                 }
             }
-
         }
         catch(IOException e){
         }
     }
 
+    void setUpConnection() throws IOException{
+        inputStream = clientSocket.getInputStream(); // gets data from client.
+        outputStream = new PrintWriter(clientSocket.getOutputStream(), true); // sends data to client.
+        bufferedReader = new BufferedReader(new InputStreamReader(inputStream)); // stores data from client.
+    }
+
+    void parseRequest() throws IOException, ArrayIndexOutOfBoundsException{
+        request = bufferedReader.readLine();
+        method = DEFAULT_METHOD;
+        path = DEFAULT_PAGE;
+
+        String[] parts = request.split(" ");
+        method = parts[0];
+        path = parts[1];
+
+        if(path.equals("/")){
+            path = DEFAULT_PAGE;
+        }
+    }
+
+    void handleRequest() throws FileNotFoundException, IOException{
+        switch(method){
+            case "GET":
+                handleGETRequest();
+                break;
+            case "POST":
+                break;
+        }
+    }
+
+    void handleGETRequest() throws FileNotFoundException, IOException{
+        File file = new File(RELATIVE_PATH + path);
+
+        if(!file.exists()){
+            outputStream.println("404 file not found!");
+        }
+
+        FileReader fileReader = new FileReader(file);
+        BufferedReader fileBuffer = new BufferedReader(fileReader); // read the file into a buffer (reused).
+        String response = HEADER; // append header to response.
+        String line;
+
+        while((line = fileBuffer.readLine()) != null){ // read until end of file.
+            response += line; // append to response.
+        }
+
+        outputStream.println(response); // send response to client.
+        fileBuffer.close();
+    }
+
+    void tearDownConnection() throws IOException{
+        bufferedReader.close();
+        outputStream.close();
+    }
+
     public static void main(String[] args){
 
         GameServer gameServer = new GameServer();
-        return 0;
+        return;
     }
 }
