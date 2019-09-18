6a3da9dde6765725deae8a58b44ecad4046eb7aa
Alex <alxbly@rams.colostate.edu>
2019-09-17 23:52:16 -0600

    user auth and action switch

--- server/GameServer.java
+++ server/GameServer.java
@@ -1,26 +1,31 @@
 import java.net.*;
 import java.io.*;
 import java.lang.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class GameServer{
 
-    static final int PORT_NUMBER = 8080;
-    static final String RELATIVE_PATH = "../client/html";
-    static final String HEADER = "HTTP/1.0 200 OK\nContent-Type: text/html\n\n";
-    static final String DEFAULT_METHOD = "GET";
-    static final String DEFAULT_PAGE = "/index.html";
+    private static final int PORT_NUMBER = 8080;
+    private static final String RELATIVE_PATH = "../client/html";
+    private static final String HEADER = "HTTP/1.0 200 OK\nContent-Type: text/html\n\n";
+    private static final String DEFAULT_METHOD = "GET";
+    private static final String DEFAULT_PAGE = "/index.html";
+    private static final double HASH_KEY = 47382;
 
-    ServerSocket serverListener;
-    Socket clientSocket;
-    InputStream inputStream;
-    PrintWriter outputStream;
-    BufferedReader bufferedReader;
+    private ServerSocket serverListener;
+    private Socket clientSocket;
+    private InputStream inputStream;
+    private PrintWriter outputStream;
+    private BufferedReader bufferedReader;
 
-    String request;
-    String method;
-    String path;
+    private String request;
+    private String method;
+    private String path;
+    private String action;
+    private Map<String, String> args = new HashMap<String, String>();
 
-    GameServer(){
+    private GameServer(){
         try{
             serverListener = new ServerSocket(PORT_NUMBER); // Set up server to listen at PORT_NUMBER.
             System.out.println("GameServer listening.");
@@ -41,13 +46,13 @@ public class GameServer{
         }
     }
 
-    void setUpConnection() throws IOException{
+    private void setUpConnection() throws IOException{
         inputStream = clientSocket.getInputStream(); // gets data from client.
         bufferedReader = new BufferedReader(new InputStreamReader(inputStream)); // stores data from client.
         outputStream = new PrintWriter(clientSocket.getOutputStream(), true); // sends data to client.
     }
 
-    void parseRequest() throws IOException, ArrayIndexOutOfBoundsException, NullPointerException{
+    private void parseRequest() throws IOException, ArrayIndexOutOfBoundsException, NullPointerException{
         request = bufferedReader.readLine();
         method = DEFAULT_METHOD;
         path = DEFAULT_PAGE;
@@ -61,17 +66,19 @@ public class GameServer{
         }
     }
 
-    void handleRequest() throws FileNotFoundException, IOException{
+    private void handleRequest() throws IOException{
+        System.out.println(request);
         switch(method){
             case "GET":
                 handleGETRequest();
                 break;
             case "POST":
+                handlePOSTRequest();
                 break;
         }
     }
 
-    void handleGETRequest() throws FileNotFoundException, IOException{
+    private void handleGETRequest() throws IOException{
         File file = new File(RELATIVE_PATH + path);
 
         if(!file.exists()){
@@ -91,13 +98,91 @@ public class GameServer{
         fileBuffer.close();
     }
 
-    void tearDownConnection() throws IOException{
+    private void handlePOSTRequest() throws IOException{
+        System.out.println("test0");
+        parsePOSTRequest();
+        System.out.println("test1");
+        handleUserAuthentication();
+        System.out.println("test2");
+        handleAction();
+        System.out.println("test3");
+    }
+
+    private void parsePOSTRequest() throws IOException{
+        int length = 0;
+        String line = "";
+
+        while((line = bufferedReader.readLine()).length() != 0){
+
+            if(line.contains("Content-Length")){
+                length = Integer.parseInt(line.split(" ")[1]);
+            }
+
+        }
+
+        char[] temp = new char[length];
+        bufferedReader.read(temp);
+        String[] kv_arr = new String(temp).split("&"); // split by key-value pair, which are separated by &;
+        for (String arg : kv_arr) {
+            String[] kv = arg.split("=");   // split by key and and value, which are separated by =
+            String key = kv[0];
+            String value = kv[1];
+            args.put(key, value);
+
+        }
+    }
+
+    private void handleAction() throws IOException{
+        action = args.get("action");
+        switch (action) {
+            case "user_registration":
+                // handleUserRegistration(); needs to register new user and redirect to index.html.
+                break;
+            case "login":
+                handleLogin();
+                break;
+            case "create_game":
+                // handleCreateGame();
+                break;
+            case "enter_game":
+                // handleEnterGame();
+                break;
+            case "forfeit_game":
+                // handleForfeitGame();
+                break;
+            case "move_piece":
+                // handleMovePiece();
+                break;
+            case "view_stats":
+                // handleViewStats();
+                break;
+        }
+    }
+
+    private void handleUserAuthentication(){
+        double username = args.get("username").hashCode() * HASH_KEY;
+        double password = args.get("password").hashCode() * HASH_KEY;
+        double pk = (username % password) * HASH_KEY;
+
+        // now use pk in SELECT statement to DB to lookup whether user exists.
+
+        // if user exists, return.
+
+        // if user does not exist, return error message.
+
+    }
+
+    private void handleLogin() throws IOException{
+        path = "/index.html";
+        handleGETRequest();
+    }
+
+    private void tearDownConnection() throws IOException{
         bufferedReader.close();
         outputStream.close();
     }
 
     public static void main(String[] args){
         GameServer gameServer = new GameServer();
-        return;
     }
 }
