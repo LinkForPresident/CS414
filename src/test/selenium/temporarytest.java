358226d823a34cc5b162c7c46bc0f30a942564df
Alex <alxbly@rams.colostate.edu>
2019-10-11 17:06:24 -0600

    works with chrome

--- src/test/selenium/temporarytest.java
+++ src/test/selenium/temporarytest.java
@@ -1,7 +1,10 @@
+package selenium;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.*;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -10,8 +13,8 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class temporarytest {
     public static void main(String[] args){
-        System.setProperty("webdriver.gecko.driver", "src/test/geckodriver");
-        FirefoxDriver driver=new FirefoxDriver();
+        System.setProperty("webdriver.chrome.driver", "selenium/chromedriver");
+        ChromeDriver driver=new ChromeDriver();
         driver.get("http://www.google.com/");
     }
 }
