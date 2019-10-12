a522a37fc96a2c39b79d4e13848c8b70066fecb9
DavidWellsTheDeveloper <dave1.t.wells@gmail.com>
2019-10-11 18:04:15 -0600

    Merge branch 'master' of /home/dave/School/Fall2019/ObjectOrientedDesign/cs414-f18-001-Team.name with conflicts.

--- client/jungle-app/src/component/Board.js
+++ client/jungle-app/src/component/Board.js
@@ -1,31 +1,47 @@
 import React from 'react';
-import Square from './Square';
 
 class Board extends React.Component {
-
     constructor(props) {
         super(props);
         this.state = {
+            background_src: "./images/dou_shou_qi_jungle_game-board.jpg",
             selected_piece: null,
         };
     }
 
-    render() {
+    setSelectedPiece(e) {
+        console.log("making request to the server for game");
+        this.setState({
+            selected_piece: e.target.value
+        })
+    }
 
+    render() {
         if (this.props.loggedIn) {
             const gameBoard = this.props.gameState.board.map((game, row_index) =>
-                <li className={'game-row col-12 col-lg-8'}>
-                    {game.map((space, column_index) =>
-                        <Square row={row_index} column={column_index} username={this.props.username} postExample={this.props.postExample} environment={space.environment} piece={space.piece} available={space.available} />
-                        )}</li>
+                <li className={'game-row'}>
+                    {game.map((piece, column_index) =>
+                        <button
+                            id={row_index.toString() + "," + column_index.toString()}
+                            value={row_index.toString() + "," + column_index.toString()}
+                            className={"game-buttons"}
+                            onClick={() => {
+                                this.props.postExample("action=move_piece&gameID=1234&username=" + this.props.username + "&password=iforgot123&row=" + row_index + "&column=" + column_index)
+                            }}
+                            // onClick={this.props.postExample()}
+                        >
+                            {piece}
+                        </button>
+                    )}</li>
             );
 
             return (
-                <div className={'Board container'}>
-
-                    <ul className={"board-ul row"}>
+                <div className={'Board'}>
+                    <p>Put the actual board here for game ({this.props.selectedGame})</p>
+                    <ul className={"board-ul"}>
                         {gameBoard}
                     </ul>
+                    <img src={this.state.background_src} alt={"board Image"}/>
                 </div>
             )
         }
