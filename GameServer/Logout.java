51cc137dacae5f8dddf18d41fd44337c14ac4adc
alexbailey97 <alxbly@rams.colostate.edu>
2019-11-02 12:03:01 -0600

    few fixes

--- GameServer/Logout.java
+++ GameServer/Logout.java
@@ -5,16 +5,17 @@ public class Logout extends Action {
     @Override
     Response executeAction(Request request) {
         Terminal.printInfo(String.format("User '%s' is attempting to log out.", request.body.get("username")));
-        String body = logout(request.header.get("cookie"));
+		String userHash = Server.calculateUserHash(request.body.get("username"), request.body.get("password"));
+        String body = logout(userHash);
         return new Response(body);
     }
 
-    private static String logout(String user_hash){
+    private static String logout(String userHash){
         // Remove user_hash from loggedInUsers list.
         String JSONResponse = "";
-        Terminal.printInfo(String.format("Attempting to log out user with user_hash: '%s'.", user_hash));
-        Server.loggedInUsers.remove(user_hash);
-        Terminal.printInfo(String.format("User with user_hash: '%s' has been logged out.", user_hash));
+        Terminal.printInfo(String.format("Attempting to log out user with user_hash: '%s'.", userHash));
+        Server.loggedInUsers.remove(userHash);
+        Terminal.printInfo(String.format("User with user_hash: '%s' has been logged out.", userHash));
         JSONResponse = String.format("{\"loggedIn\": %b}", false);
         return JSONResponse;
     }
