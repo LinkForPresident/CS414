e41fa25e428d8dba1e1338e85c5e9cdf93c3bc48
Timothy Rooney <tprooney@rams.colostate.edu>
2019-12-02 11:48:29 -0700

    Updated rules page to have proper formatting and information.

--- client/jungle-app/src/component/GameRules.js
+++ client/jungle-app/src/component/GameRules.js
@@ -6,131 +6,115 @@ class GameRules extends React.Component {
     // Static page with description
     render() {
         return (
-            <div className={'GaneRules'}>
+            <div className={'GameRules'}>
                 <h2>Rules of the Game of Jungle</h2>
 
                 <h3>Pieces</h3>
                 <p>Each player has eight pieces, which are different animals, with different degrees of power indicated by number.</p>
 
-                <Card className={"pieceDescriptionCard"} bg={"primary"} border={"light"}>
-                    <Card.Img variant="top" style={{width: '9em'}} src={require("./images/b1.png")} />
-                    <Card.Body>
-                        <Card.Title>Rat</Card.Title>
-                    </Card.Body>
-                </Card>
+                <div className={'pieces'}>
+                    <Card className={"pieceDescriptionCard"} bg={"primary"} border={"light"}>
+                        <Card.Img variant="top" style={{width: '7em'}} src={require("./images/b1.png")} />
+                        <Card.Body>
+                            <Card.Title>Rat</Card.Title>
+                        </Card.Body>
+                    </Card>
 
-                <Card className={"pieceDescriptionCard"} bg={"primary"} border={"light"}>
-                    <Card.Img variant="top" style={{width: '9em'}} src={require("./images/b2.png")} />
-                    <Card.Body>
-                        <Card.Title>Cat</Card.Title>
-                    </Card.Body>
-                </Card>
-                <Card className={"pieceDescriptionCard"} bg={"primary"} border={"light"}>
-                    <Card.Img variant="top" style={{width: '9em'}} src={require("./images/b3.png")} />
-                    <Card.Body>
-                        <Card.Title>Wolf</Card.Title>
-                    </Card.Body>
-                </Card>
+                    <Card className={"pieceDescriptionCard"} bg={"primary"} border={"light"}>
+                        <Card.Img variant="top" style={{width: '7em'}} src={require("./images/b2.png")} />
+                        <Card.Body>
+                            <Card.Title>Cat</Card.Title>
+                        </Card.Body>
+                    </Card>
+                    <Card className={"pieceDescriptionCard"} bg={"primary"} border={"light"}>
+                        <Card.Img variant="top" style={{width: '7em'}} src={require("./images/b3.png")} />
+                        <Card.Body>
+                            <Card.Title>Wolf</Card.Title>
+                        </Card.Body>
+                    </Card>
 
-                <Card className={"pieceDescriptionCard"} bg={"primary"} border={"light"}>
-                    <Card.Img variant="top" style={{width: '9em'}} src={require("./images/b4.png")} />
-                    <Card.Body>
-                        <Card.Title>Dog</Card.Title>
-                    </Card.Body>
-                </Card>
-                <Card className={"pieceDescriptionCard"} bg={"primary"} border={"light"}>
-                    <Card.Img variant="top" style={{width: '9em'}} src={require("./images/b5.png")} />
-                    <Card.Body>
-                        <Card.Title>Panther</Card.Title>
-                    </Card.Body>
-                </Card>
+                    <Card className={"pieceDescriptionCard"} bg={"primary"} border={"light"}>
+                        <Card.Img variant="top" style={{width: '7em'}} src={require("./images/b4.png")} />
+                        <Card.Body>
+                            <Card.Title>Dog</Card.Title>
+                        </Card.Body>
+                    </Card>
+                    <Card className={"pieceDescriptionCard"} bg={"primary"} border={"light"}>
+                        <Card.Img variant="top" style={{width: '7em'}} src={require("./images/b5.png")} />
+                        <Card.Body>
+                            <Card.Title>Panther</Card.Title>
+                        </Card.Body>
+                    </Card>
 
-                <Card className={"pieceDescriptionCard"} bg={"primary"} border={"light"}>
-                    <Card.Img variant="top" style={{width: '9em'}} src={require("./images/b6.png")} />
-                    <Card.Body>
-                        <Card.Title>Tiger</Card.Title>
-                    </Card.Body>
-                </Card>
+                    <Card className={"pieceDescriptionCard"} bg={"primary"} border={"light"}>
+                        <Card.Img variant="top" style={{width: '7em'}} src={require("./images/b6.png")} />
+                        <Card.Body>
+                            <Card.Title>Tiger</Card.Title>
+                        </Card.Body>
+                    </Card>
 
-                <Card className={"pieceDescriptionCard"} bg={"primary"} border={"light"}>
-                    <Card.Img variant="top" style={{width: '9em'}} src={require("./images/b7.png")} />
-                    <Card.Body>
-                        <Card.Title>Lion</Card.Title>
-                    </Card.Body>
-                </Card>
+                    <Card className={"pieceDescriptionCard"} bg={"primary"} border={"light"}>
+                        <Card.Img variant="top" style={{width: '7em'}} src={require("./images/b7.png")} />
+                        <Card.Body>
+                            <Card.Title>Lion</Card.Title>
+                        </Card.Body>
+                    </Card>
 
-                <Card className={"pieceDescriptionCard"} bg={"primary"} border={"light"}>
-                    <Card.Img variant="top" style={{width: '9em'}} src={require("./images/b8.png")} />
-                    <Card.Body>
-                        <Card.Title>Elephant</Card.Title>
-                    </Card.Body>
-                </Card>
+                    <Card className={"pieceDescriptionCard"} bg={"primary"} border={"light"}>
+                        <Card.Img variant="top" style={{width: '7em'}} src={require("./images/b8.png")} />
+                        <Card.Body>
+                            <Card.Title>Elephant</Card.Title>
+                        </Card.Body>
+                    </Card>
+                </div>
                 <br style={{clear: "left"}}/>
                 <ul>
                     <li>The rat is the only animal that may go onto a water square, and cannot be killed while in the water.</li>
                     <li>The rat can capture and Elephant, but only from a land square (not a water tile)</li>
+                    <li>The Lion and the Tiger can jump over the river horizontally or vertically, landing on the next land tile on the other side. This cannot happen if either color rat is in between the land tiles.</li>
                 </ul>
 
 
                 <h3>Tile Environments</h3>
                 <Card className={"tileDescriptionCard"} bg={"dark"} border={"success"}>
-                    <Card.Img variant="top" style={{width: '9em'}} src={require("./images/trap.png")} />
+                    <Card.Img variant="top" style={{width: '7em', margin:'10px'}} src={require("./images/den.png")} />
                     <Card.Body>
-                        <Card.Title>Trap</Card.Title>
-                        <Card.Text>This is the card Text</Card.Text>
+                        <Card.Title>Den</Card.Title>
+                        <Card.Text>The first player to get one of their animals into the opposing den wins. Animals cannot move into their own den.</Card.Text>
                     </Card.Body>
                 </Card>
 
                 <Card className={"tileDescriptionCard"} bg={"dark"} border={"success"}>
-                    <Card.Img variant="top" style={{width: '9em'}} src={require("./images/water.png")} />
+                    <Card.Img variant="top" style={{width: '7em', margin:'10px'}} src={require("./images/trap.png")} />
                     <Card.Body>
                         <Card.Title>Trap</Card.Title>
-                        <Card.Text>This is the card Text</Card.Text>
+                        <Card.Text>Three traps surround the Den. Any animal in an opposing trap tile temporarily reduces their rank to 0 until they exit the trap.</Card.Text>
                     </Card.Body>
                 </Card>
 
                 <Card className={"tileDescriptionCard"} bg={"dark"} border={"success"}>
-                    <Card.Img variant="top" style={{width: '9em'}} src={require("./images/den.png")} />
+                    <Card.Img variant="top" style={{width: '7em', margin:'10px'}} src={require("./images/water.png")} />
                     <Card.Body>
-                        <Card.Title>Trap</Card.Title>
-                        <Card.Text>This is the card Text</Card.Text>
+                        <Card.Title>River</Card.Title>
+                        <Card.Text>Acts as a tile that cannot be moved through, except by the rat. The Lion and Tiger can also leap over the river.</Card.Text>
                     </Card.Body>
                 </Card>
 
+                <br style={{clear: "left"}}/>
 
-                {/*consider removing or modifying the following paragraph*/}
-                <p style={{clear: "left"}}>The Animals depicted on the pieces shown here are very interesting abstractions. Your set
-                    may look like this, or may have the same animals depicted in a different style — and the
-                    same animals may be depicted differently on the board as well. You may want to pencil the
-                    pieces’ value numbers, 1 through 8, on the underside of each piece, or keep this chart on
-                    hand, to help you become acquainted with the pieces as you play.</p>
+                <h2>Object of the Game</h2>
+                <p> To win the game, one player must successfully move any animal into the Den of the opponent.</p>
 
-                <p>Object of the Game</p>
+                <h2>Movement of the Pieces</h2>
+                <p>Blue pieces have the first move. All pieces can move one space either forward, backward, left or right. The pieces never move diagonally. Some pieces have special moves, as described above.</p>
 
-                <p> To win the game, one player must successfully move any animal into the Den of the opponent. (see Den in the diagram above</p>
+                <h2>Captures</h2>
+                <p>An animal is captured (or “eaten”) by an opposing animal moving onto its square. The attacking animal must be of equal or higher power than the one being captured. For instance, the Tiger (6) can capture the Tiger (6), Leopard (5) or Dog (4), but the Dog can not capture the Leopard or Tiger.</p>
 
-                <p>Movement of the Pieces</p>
-
-                <p>The black (or darker) pieces have the first move. All pieces have the same basic move, although some have special powers (described below). The basic move is just one space either forward, backward, left or right. The pieces never move diagonally.</p>
-
-                <p>Captures</p>
-
-                <p>An animal is captured (or “eaten”) by an opposing animal moving onto its square, as in chess or Stratego. But the attacking animal must be of equal or higher power than the one being captured. For instance, the Tiger (6) can capture the Tiger (6), Leopard (5) or Dog (4), but the Dog can not capture the Leopard or Tiger.</p>
-
-                <p>Special Powers</p>
-
-                <p>1) The Rat, although it is the least powerful piece, has the power to capture the Elephant. The Elephant can not capture the Rat. It is said that this is because the rat can creep in under the Elephant’s ear and eat his brain (!).</p>
-
-                <p>2) The Rat, and no other animal, can move freely in the water. It can not, however, attack the Elephant from the water.</p>
-
-                <p>3) Both the Lion and the Tiger can jump over the water, moving from one bank straight forward, backward, left or right (like a rook in chess) to the first square of dry land on the other side. They may capture in this move as well. The Lion and Tiger may not, however, jump over a rat if it is in the way, in the water.</p>
-
-                <p> The Traps</p>
-
+                <h2> The Traps</h2>
                 <p>Each side has three Trap squares surrounding its Den. A player may move on and off of his own Trap squares with no effect. If, however, a player moves onto the opponent’s trap square, that piece loses all of its power, and may be captured by any of the defending pieces.</p>
 
-                <p>The Den</p>
-
+                <h2>The Den</h2>
                 <p> Animals are not allowed to move into their own Dens. When an animal moves into the opponent’s Den, it has won the game.</p>
             </div>
         )
